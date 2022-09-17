41056ce1743d0f7ec6825cc4c2740c316e9143f6
SU-T <guitaristpeet@gmail.com>
2022-09-16 18:05:58 -0700

    add square animation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container')
 let rowHtml = '';
 
+// Create row and column of squares
 for (i = 0; i < 10; i++) {
     let squareHtml = '';
     for (j = 0; j < 10; j++) {
@@ -9,3 +10,16 @@ for (i = 0; i < 10; i++) {
     rowHtml += `<div class="row">${squareHtml}</div>`;
 }
 container.innerHTML = rowHtml;
+
+// Square animation
+const square = document.querySelectorAll('.square');
+gsap.from(square, {
+    opacity: 0.1,
+    ease: 'linear',
+    repeat: -1,
+    yoyo: true,
+    stagger: {
+        amount: 3,
+        from: 'edges',
+    }
+}, '+=0.5') 
